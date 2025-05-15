119a337936764bdacdca3feadf8f1b768f085684
nguyen7925 <nguyenchinguyen7925@gmail.com>
2025-05-15 21:54:27 +0700

    có models và controller

--- DATN-API/Migrations/20250515145334_adad.Designer.cs
+++ DATN-API/Migrations/20250515145334_adad.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DATN_API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250515132402_asssss")]
-    partial class asssss
+    [Migration("20250515145334_adad")]
+    partial class adad
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
--- DATN-API/Migrations/20250515145334_adad.cs
+++ DATN-API/Migrations/20250515145334_adad.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace DATN_API.Migrations
 {
     /// <inheritdoc />
-    public partial class IntialCreate : Migration
+    public partial class adad : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
