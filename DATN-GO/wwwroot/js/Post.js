556f235058d001cfdb0b0017e76d0ad27be2f666
nguyen7925 <nguyenchinguyen7925@gmail.com>
2025-05-24 20:43:48 +0700

    update post

---
+++ DATN-GO/wwwroot/js/Post.js
@@ -0,0 +1,31 @@
+﻿    document.addEventListener('DOMContentLoaded', function () {
+        const imageInput = document.getElementById('imageInput');
+    const previewImage = document.getElementById('previewImage');
+    const previewContainer = document.getElementById('previewContainer');
+    const fileNameDisplay = document.getElementById('fileNameDisplay');
+
+    imageInput.addEventListener('change', function (event) {
+            const file = event.target.files[0];
+
+    if (file) {
+                const reader = new FileReader();
+
+    reader.onload = function (e) {
+        previewImage.src = e.target.result;
+    previewContainer.style.display = 'block';
+                };
+
+    reader.readAsDataURL(file);
+    fileNameDisplay.textContent = file.name;
+            } else {
+        previewImage.src = "#";
+    previewContainer.style.display = 'none';
+    fileNameDisplay.textContent = '';
+            }
+        });
+    });
+
+    function setDeleteUrl(postId) {
+        var form = document.getElementById('deleteForm');
+    form.action = '/Seller/PostArticle/Delete/' + postId;
+    }
